b62eeaa5e3eadcd90eb99b654d3f7dc2ee24776b
Vishesh chintan <vishesh.chintan1206@gmail.com>
2022-12-21 11:56:25 +0530

    Screen_awaker.py

---
+++ Screen Awaker.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time 
+
+pyautogui.FAILSAFE = False
+while True:
+    time.sleep(10)
+    for i in range(0,200):
+        pyautogui.moveTo(500 ,i*5)
+    for i in range(0,3):
+        pyautogui.press("Shift")
