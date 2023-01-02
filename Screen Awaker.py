2e0cedb536c2baed64ed74e1978800d23dfd2eec
Vishesh chintan <vishesh.chintan1206@gmail.com>
2023-01-02 21:11:54 +0530

    Update Screen Awaker.py

--- Screen Awaker.py
+++ Screen Awaker.py
@@ -3,8 +3,9 @@ import time
 
 pyautogui.FAILSAFE = False
 while True:
-    time.sleep(10)
+    time.sleep(100)
     for i in range(0,200):
         pyautogui.moveTo(500 ,i*5)
     for i in range(0,3):
         pyautogui.press("Shift")
+        exit()
